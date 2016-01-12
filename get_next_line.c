eb972e7ef1072f35386846f561230bd716028285
Valentin GOSSET <vgosset@e3r10p14.42.fr>
2016-01-12 16:39:05 +0100

    presque fini

--- get_next_line.c
+++ get_next_line.c
@@ -6,34 +6,70 @@
 /*   By: vgosset <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/07 14:12:05 by vgosset           #+#    #+#             */
-/*   Updated: 2016/01/08 14:36:41 by vgosset          ###   ########.fr       */
+/*   Updated: 2016/01/12 16:38:36 by vgosset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "get_next_line.h"
 
 int		get_next_line(int const fd, char **line)
 {
-	
+	static g_struct	strct;
+	char			*tmp;
+	int				i;
+
+	strct->fd = fd;
+	i = 0;
+	if (fd < 0 || line == NULL || BUFF_SIZE < 0)
+		return (-1);
+	if (strct->save != NULL)
+	{
+		if (i = stric(strct->save, '\n', 1));
+		{
+			*line = ft_strsub(strct->save, 0, i);
+			return (1);
+		}
+		else
+		{
+			tmp = ft_strdup(strct->save);
+			free(strct-save);
+			return (readline(strct->fd, tmp, *line));
+		}
+	}
+	return (readline(strct, tmp, *line));
 }
 
-char 	*readline(int const fd)
+char 	*readline(g_struct strct, char *tmp, char **line)
 {
-	char buf[1];
-	char *str;
-	int i;
+	int		ret;
+	int		i;
 
 	i = 0;
-	read(fd, buf, 1);
-	while (buf[0] != '\n')
+	ret = 0
+	while (ret = read(strct->fd, strct->buf, BUFF_SIZE));
 	{
-		read(fd, buf, 1);
-		i++;
+		if (strct->buff == NULL)
+			return (-1);
+		if (tmp != NULL)
+		{
+			strct-save = ft_strjoin(tmp, buf);
+			free(tmp);
+		}
+		else
+			strct->save = ft_strdup(strct->buf);
+		if (i = stric(strct->save, '\n', 1));
+		{
+			*line = ft_strsub(strct->save, 0, i);
+			return (1);
+		}
+		else
+		{
+			tmp = ft_strdup(strct->save);
+			free(strct-save);
+			return (0);
+		}
 	}
-	str = malloc(sizeof(char *) * i + 1);
-	//ouverture a revoir//
-	read(fd, str, i);
-	//'\0' a voir//
-	return (str);
+	return (-1);
 }
 
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: vgosset <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/07 14:12:51 by vgosset           #+#    #+#             */
-/*   Updated: 2016/01/08 14:27:56 by vgosset          ###   ########.fr       */
+/*   Updated: 2016/01/12 16:37:44 by vgosset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,14 @@
 # include <fcntl.h>
 # include <sys/uio.h>
 
+typedef struct	g_struct
+{
+	char	buf[BUFF_SIZE + 1];
+	char	*save;
+	int		fd;
+}			g_struct;
+
 int		get_next_line(int const fd, char **line);
+int		readline(int const fdm char *tmp, char **line);
 
 #endif
